43e3d19f0a60d5932fdb78c42cf57f869a7293dd
GUSTAVO PINHEIRO MAIA <gustavo.pmaia@senacsp.edu.br>
2023-09-15 11:30:18 -0300

    fix fonts

--- script.js
+++ script.js
@@ -13,21 +13,25 @@ const date = new Date()
 let day = date.getDate()
 let month = date.getMonth()
 let year = date.getFullYear()
-const now = `${day}-${month}-${year}`
-let id = `ID: 71e39802-0ebc-4eca-8116-1ca928627717`
+const now = `${day}-${month + 1}-${year}`
+let id = `71e39802-0ebc-4eca-8116-1ca928627717`
 
 function drawImage() {
   // ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-  ctx.font = '20px monotype corsiva'
+  ctx.font = '20px monotype arial'
   ctx.fillStyle = '#605746'
+  // Nome Certificado
   ctx.fillText(nameInput.value, 140, 170)
-  ctx.font = '17px monotype corsiva'
+  ctx.font = '17px monotype arial'
+  // Data certificado
   ctx.fillText(now, 125, 280)
-  ctx.font = '10px monotype corsiva'
+  ctx.font = '10px monotype arial'
   ctx.fillStyle = '#8f8c88'
-  ctx.fillText(id, 5, 345)
+  ctx.fillText(`ID: ${crypto.randomUUID}`, 5, 345)
 }
+
+console.log(crypto.randomUUID)
 
 nameInput.addEventListener('input', function () {
   drawImage()
